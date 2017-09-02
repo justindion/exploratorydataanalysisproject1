063cf7fb0af3c54f27d3f6f2603b81e6b5f76fa6
justindion <justinshirley@rocketmail.com>
2017-09-02 20:21:13 +1000

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -17,7 +17,6 @@ df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
 df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
 
 #plot 1
-
 plot1 <- function() {
         hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
         dev.copy(png, file="plot1.png", width=480, height=480)
